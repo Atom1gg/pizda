b1441cbbd1dadad28b917b4fc982b0943e61ab78
AtomoV <Zursteko@gmail.com>
2025-08-26 14:32:45 +0300

    Update UmbrellaHub.lua

--- UmbrellaHub.lua
+++ UmbrellaHub.lua
@@ -1,9 +1,12 @@
 local GAMES = {
-    CounterBlox = 301549746, -- Counter Blox
+    MainGame = 1480424328, -- Главная игра
+    Division = 301549746,  -- Подразделение (Counter Blox)
 }
 
 -- Проверка текущего PlaceId
-if game.PlaceId == GAMES.CounterBlox then
+if game.PlaceId == GAMES.MainGame then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/main_game.lua"))()
+elseif game.PlaceId == GAMES.Division then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua"))()
 else
     return
