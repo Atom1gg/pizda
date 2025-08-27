4e84e02d3671c87b40859b22778e0dc63d3be1e2
AtomoV <Zursteko@gmail.com>
2025-08-27 21:14:03 +0300

    Update UmbrellaHub.lua

--- UmbrellaHub.lua
+++ UmbrellaHub.lua
@@ -1,3 +1,17 @@
+-- функция для получения имени инжектора
+local function getExecutor()
+    if identifyexecutor then
+        return identifyexecutor()
+    elseif getexecutorname then
+        return getexecutorname()
+    else
+        return "Unknown"
+    end
+end
+
+local executor = getExecutor()
+
+-- список поддерживаемых игр
 local GAMES = {
     [1480424328]  = {name = "Counter Blox Unranked", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua"},
     [301549746]   = {name = "Counter Blox", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua"},
@@ -6,8 +20,10 @@ local GAMES = {
     [3978370137]  = {name = "GPO Main Sea 1", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline"},
 }
 
+-- твой вебхук
 local WEBHOOK_URL = "https://discordapp.com/api/webhooks/1410324453975658668/61-dJPlKwlAQzfjFcKtdMH2aCyCtvVN1MsE-X_dR55TBFE2L_5APBUcgf9B1P7U6AKK5"
 
+-- отправка в дискорд
 local function sendToDiscord(msg)
     local HttpService = game:GetService("HttpService")
     local data = HttpService:JSONEncode({content = msg})
@@ -22,18 +38,17 @@ local function sendToDiscord(msg)
             },
             Body = data
         })
-    else
     end
 end
 
--- логика
+-- логика проверки
 local player = game.Players.LocalPlayer
 local info = GAMES[game.PlaceId]
 
 if info then
-    sendToDiscord("✅ " .. player.Name .. " заинжектил UmbrellaHub в игру: **" .. info.name .. "** (PlaceId: " .. game.PlaceId .. ")")
+    sendToDiscord("✅ " .. player.Name .. " (".. player.UserId ..") заинжектил UmbrellaHub в игру: **" .. info.name .. "** (PlaceId: " .. game.PlaceId .. ")\nИнжектор: **" .. executor .. "**")
     loadstring(game:HttpGet(info.url))()
 else
-    sendToDiscord("❌ " .. player.Name .. " пытался заинжектить UmbrellaHub в неподдерживаемую игру (PlaceId: " .. game.PlaceId .. ")")
+    sendToDiscord("❌ " .. player.Name .. " (".. player.UserId ..") пытался заинжектить UmbrellaHub в неподдерживаемую игру (PlaceId: " .. game.PlaceId .. ")\nИнжектор: **" .. executor .. "**")
     player:Kick("UmbrellaHub does not support this game.")
 end
