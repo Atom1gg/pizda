3889522061b8bf79c917851d934abb2a7d33336d
AtomoV <Zursteko@gmail.com>
2025-08-27 21:07:42 +0300

    Update UmbrellaHub.lua

--- UmbrellaHub.lua
+++ UmbrellaHub.lua
@@ -1,14 +1,39 @@
 local GAMES = {
-    [1480424328] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua", -- counterblox unranked
-    [301549746]  = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua", -- counterblox 
-    [6360478118] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline", -- gpo universe hub
-    [11424731604] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline", -- gpo battle royale
-    [3978370137] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline", -- gpo main sea 1
+    [1480424328]  = {name = "Counter Blox Unranked", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua"},
+    [301549746]   = {name = "Counter Blox", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua"},
+    [6360478118]  = {name = "GPO Universe Hub", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline"},
+    [11424731604] = {name = "GPO Battle Royale", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline"},
+    [3978370137]  = {name = "GPO Main Sea 1", url = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline"},
 }
 
-local scriptUrl = GAMES[game.PlaceId]
-if scriptUrl then
-    loadstring(game:HttpGet(scriptUrl))()
+local WEBHOOK_URL = "https://discordapp.com/api/webhooks/1410324453975658668/61-dJPlKwlAQzfjFcKtdMH2aCyCtvVN1MsE-X_dR55TBFE2L_5APBUcgf9B1P7U6AKK5"
+
+local function sendToDiscord(msg)
+    local HttpService = game:GetService("HttpService")
+    local data = HttpService:JSONEncode({content = msg})
+
+    local req = (syn and syn.request) or (http and http.request) or (http_request) or (request)
+    if req then
+        req({
+            Url = WEBHOOK_URL,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = data
+        })
+    else
+    end
+end
+
+-- логика
+local player = game.Players.LocalPlayer
+local info = GAMES[game.PlaceId]
+
+if info then
+    sendToDiscord("✅ " .. player.Name .. " заинжектил UmbrellaHub в игру: **" .. info.name .. "** (PlaceId: " .. game.PlaceId .. ")")
+    loadstring(game:HttpGet(info.url))()
 else
-    game.Players.LocalPlayer:Kick("UmbrellaHub does not support this game.")
+    sendToDiscord("❌ " .. player.Name .. " пытался заинжектить UmbrellaHub в неподдерживаемую игру (PlaceId: " .. game.PlaceId .. ")")
+    player:Kick("UmbrellaHub does not support this game.")
 end
