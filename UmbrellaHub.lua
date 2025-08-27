16759c3fc4b736253a47870590b791c936d69203
AtomoV <Zursteko@gmail.com>
2025-08-27 18:28:47 +0300

    Update UmbrellaHub.lua

--- UmbrellaHub.lua
+++ UmbrellaHub.lua
@@ -3,6 +3,7 @@ local GAMES = {
     [301549746]  = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/counterblox.lua", -- counterblox 
     [6360478118] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline", -- gpo universe hub
     [11424731604] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline", -- gpo battle royale
+    [3978370137] = "https://raw.githubusercontent.com/Atom1gg/pizda/refs/heads/main/games/grandpieceonline", -- gpo main sea 1
 }
 
 local scriptUrl = GAMES[game.PlaceId]
