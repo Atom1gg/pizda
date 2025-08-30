6a8b52216d86401a293e702cc81032f99c6d84bf
AtomoV <Zursteko@gmail.com>
2025-08-30 20:09:36 +0300

    Update UmbrellaHub.lua

--- UmbrellaHub.lua
+++ UmbrellaHub.lua
@@ -1,3 +1,5 @@
+-- ⚡ UmbrellaHub Loader с АНТИ-СЛИВОМ и АНТИ-ДЕБАГОМ ⚡
+
 -- функция для получения имени инжектора
 local function getExecutor()
     if identifyexecutor then
@@ -10,6 +12,7 @@ local function getExecutor()
 end
 
 local executor = getExecutor()
+local player = game.Players.LocalPlayer
 
 -- список поддерживаемых игр
 local GAMES = {
@@ -30,6 +33,10 @@ local TRASH_EXECUTORS = {
 -- твой вебхук
 local WEBHOOK_URL = "https://discordapp.com/api/webhooks/1410324453975658668/61-dJPlKwlAQzfjFcKtdMH2aCyCtvVN1MsE-X_dR55TBFE2L_5APBUcgf9B1P7U6AKK5"
 
+----------------------------------------------------------
+-- АНТИ-СЛИВ И АНТИ-ДЕБАГ
+----------------------------------------------------------
+
 -- отправка в дискорд
 local function sendToDiscord(msg)
     local HttpService = game:GetService("HttpService")
@@ -40,16 +47,57 @@ local function sendToDiscord(msg)
         req({
             Url = WEBHOOK_URL,
             Method = "POST",
-            Headers = {
-                ["Content-Type"] = "application/json"
-            },
+            Headers = {["Content-Type"] = "application/json"},
             Body = data
         })
     end
 end
 
--- логика проверки
-local player = game.Players.LocalPlayer
+-- что делать с крысой
+local function punish(reason)
+    sendToDiscord("⚠️ [ANTI-DEBUG] Игрок **" .. player.Name .. " ("..player.UserId..")** пытался " .. reason)
+    player:Kick("dumb child trying to find something interesting?)))")
+    task.wait(0.1)
+    while true do end -- зависание
+end
+
+-- проверка аргументов
+local function antiLeakCheck(...)
+    local args = {...}
+    for _, v in next, args do
+        v = tostring(v)
+        if v:find("https://") or v:find("http://") or v:find("webhook") then
+            punish("дебажить/логнуть ссылки или вебхук")
+        end
+    end
+end
+
+-- хук опасных функций
+local protectedFuncs = {print, warn, error, rconsoleprint, rconsolewarn, rconsoleerr, setclipboard}
+for _, fn in next, protectedFuncs do
+    if fn then
+        local old
+        old = hookfunction(fn, newcclosure(function(...)
+            antiLeakCheck(...)
+            return old(...)
+        end))
+    end
+end
+
+-- метатаблица для _G
+setmetatable(_G, {
+    __newindex = function(t, i, v)
+        if tostring(i):lower():find("id") or tostring(i):lower():find("webhook") then
+            punish("подменить глобальные переменные")
+        end
+        rawset(t, i, v)
+    end
+})
+
+----------------------------------------------------------
+-- ЛОГИКА ЗАПУСКА СКРИПТА
+----------------------------------------------------------
+
 local info = GAMES[game.PlaceId]
 
 -- 1. Проверка игры
